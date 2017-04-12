2520509e8de9669c2869b38502824334c65f5e51
Megamaiku <megamancito@gmail.com>
2017-04-12 11:31:53 -0700

    Create css-stuff.js

---
+++ css-stuff.js
@@ -0,0 +1,5 @@
+// Managed to follow my way down the rabbit hole to select a difficult-to-select element, and then changed its margin-top value. Phew!
+
+$(function () {
+    var presentationToggle = $(document.getElementById("presentation-toggle").nextElementSibling.children[2]).css("marginTop", "-10px");
+})
